a0e40c8a722bb59ae572351b85509ece3ec7d6e8
Atul Dhadse <47805006+atul-dhadse@users.noreply.github.com>
2020-06-17 21:28:45 +0530

    Update snake_01.py

--- snakeGame/snake_01.py
+++ snakeGame/snake_01.py
@@ -1,187 +1,219 @@
-import pygame,sys,random
+import pygame
+import sys
+import random
+import math
+from pygame.locals import *
 
-pygame.init()
+WINDOWWIDTH = 750
+WINDOWHEIGHT = 600
 
-game_display = pygame.display.set_mode((800,600))
-pygame.display.set_caption("snake")
+CELLSIZE = 15
+FPS = 15
 
-clock = pygame.time.Clock()
+assert (WINDOWWIDTH % CELLSIZE == 0)
+assert (WINDOWHEIGHT % CELLSIZE == 0)
 
-#function for msg on screen
+CELLWIDHT = int(WINDOWWIDTH/CELLSIZE)
+CELLHEIGHT = int(WINDOWHEIGHT/CELLSIZE)
 
-font = pygame.font.SysFont(None, 35)
-def display_text(msg, color):
-    text = font.render(msg, True, color)
-    return text, text.get_rect()
+#colors
+DARKGREEN = (0  ,153,  0)
+BLACK     = (0  ,0  ,  0)
+GREY      = (40 ,40 , 40)
+RED       = (255,0  ,  0)
+GREEN     = (128,255,128)
+WHITE     = (255,255,255)
 
-red = (255,0,0)
+BGCOLOR  = BLACK
 
-def screen_msg(msg,color,y=300):
-    surface_text,text_rect = display_text(msg, color)
-    text_rect.center = 400,y
-    game_display.blit(surface_text,text_rect)
+UP = "up"
+DOWN = "down"
+LEFT = "left"
+RIGHT = "right"
 
-#=======================================================================
+def main():
+    pygame.init()
+    global DISPLAYSURF, FPSCLOCK
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    FPSCLOCK = pygame.time.Clock()
 
-def score(points):
-    text = font.render("SCORE: " + str(points), True, (0,140,0))
-    game_display.blit(text , [10,10])
+    showStartScreen()
 
-
-direction = "right"
-green = (0,140,0)
-def start():
     while True:
-        screen_msg("welcome to snake game!",green,200)
-        screen_msg("press 'c' for continue, 'q' for quit",green,300)
-        screen_msg("you can use 'space' to pause the game",green,400)
+        runGame()
+        gameOverScreen()
 
+def runGame():
+    
+    HEAD = 0
+    start_x = random.randint(5, CELLWIDHT - 5)
+    start_y = random.randint(5, CELLHEIGHT - 5)
+
+    direction = RIGHT
+
+    wormCords = [
+        {'x' : start_x    , 'y' : start_y},
+        {'x' : start_x - 1, 'y' : start_y},
+        {'x' : start_x - 2, 'y' : start_y}
+    ]
+
+    apple = getRandomLocation()
+
+
+    while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_c:
-                    run_game()
-                elif event.key == pygame.K_q:
-                    sys.exit()
+            if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+                terminate()
+            if event.type == KEYDOWN:
+                if event.key == K_UP and direction != DOWN:
+                    direction = UP
+                elif event.key == K_DOWN and direction != UP:
+                    direction = DOWN
+                elif event.key == K_LEFT and direction != RIGHT:
+                    direction = LEFT
+                elif event.key == K_RIGHT and direction != LEFT:
+                    direction = RIGHT
+
+        if direction == UP:
+            HEAD = {'x' : wormCords[0]['x'], 'y' : wormCords[0]['y'] - 1}
+            # start_y -= 1
+        elif direction == DOWN:
+            HEAD = {'x' : wormCords[0]['x'], 'y' : wormCords[0]['y'] + 1}
+            # start_y += 1
+        elif direction == LEFT:
+            HEAD = {'x' : wormCords[0]['x'] - 1, 'y' : wormCords[0]['y']}
+            # start_x -= 1
+        elif direction == RIGHT:
+            HEAD = {'x' : wormCords[0]['x'] + 1, 'y' : wormCords[0]['y']}
+            # start_x += 1
+
+        if wormCords[0]['x'] == -1 or wormCords[0]['x'] == CELLWIDHT or wormCords[0]['y'] == -1 or wormCords[0]['y'] == CELLHEIGHT:
+            return
+
+        for worm in wormCords[1:]:
+            if worm['x'] == wormCords[0]['x'] and worm['y'] == wormCords[0]['y']:
+                return
+
+        wormCords.insert(0, HEAD)
+
+        if apple['x'] == wormCords[0]['x'] and apple['y'] == wormCords[0]['y']:
+            apple = getRandomLocation()
+        else:
+            del wormCords[-1]
+
+        DISPLAYSURF.fill(BGCOLOR)
+        drawGrids()
+        drawWorm(wormCords)
+        drawApple(apple)
+        drawScores(len(wormCords) - 3)
+
         pygame.display.update()
-        clock.tick(5)
+        FPSCLOCK.tick(FPS)
 
-def game_pause():
+def drawScores(scores):
+    FONT = pygame.font.Font("freesansbold.ttf", 20)
+    FONTSURF = FONT.render("score : " + str(scores), True, WHITE)
+    FONTRECT = FONTSURF.get_rect()
+    FONTRECT.topleft = (WINDOWWIDTH - 100, 10)
+    DISPLAYSURF.blit(FONTSURF, FONTRECT)
 
-    paused = True
+def drawApple(apple):
+    pygame.draw.rect(DISPLAYSURF, RED, (apple['x'] * CELLSIZE, apple['y'] * CELLSIZE, CELLSIZE, CELLSIZE))
 
-    while paused: 
-        screen_msg("paused",red,300)
-        screen_msg("press 'c' for continue or 'q' to quit",red,350)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_c:
-                    paused = False
-                elif event.key == pygame.K_q:
-                    sys.exit()
-                elif event.key == pygame.K_SPACE:
-                    game_pause()
-
-        pygame.display.update()
-        clock.tick(5)
+def getRandomLocation():
+    return {'x' : random.randint(0, CELLWIDHT - 1), 'y' : random.randint(0, CELLHEIGHT - 1)}
 
 
-
-def run_game():
-    random_food_x = round(random.randrange(20,770)/10.0)*10.0
-    random_food_y = round(random.randrange(20,570)/10.0)*10.0
-    lead_x = 300
-    lead_y = 200
-    lead_x_change = 0
-    lead_y_change = 0
-    FPS = 30
-    white = (255,255,255)
-    black = (0,0,0)
-    red = (255,0,0)
-    game_over = False
-    game_close = False
-    # pygame.init()
-    snake_list = []
-    snake_len = 1
-
-    global direction
-
-    img = pygame.image.load("snakehead1.png")
-    apple_img = pygame.image.load("snake_food.png")
+def drawGrids():
+    for i in range(CELLHEIGHT):
+        pygame.draw.line(DISPLAYSURF, GREY, (0, CELLSIZE * i), (WINDOWWIDTH, CELLSIZE * i))
+    for i in range(CELLWIDHT):
+        pygame.draw.line(DISPLAYSURF, GREY, (CELLSIZE * i, 0), (CELLSIZE * i, WINDOWHEIGHT))
 
 
-    def snake(snake_list, img):
-        if direction == "right":
-            head = pygame.transform.rotate(img,270)
-        elif direction == "left":
-            head = pygame.transform.rotate(img,90)
-        elif direction == "up":
-            head = pygame.transform.rotate(img,0)
-        elif direction == "down":
-            head = pygame.transform.rotate(img,180)
-         
-        game_display.blit(head , [snake_list[-1][0],snake_list[-1][1]])
-        for XnY in snake_list[:-1] :
-            pygame.draw.rect(game_display, white, [XnY[0],XnY[1],10,10])
-
-    while not game_close:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    direction = "right"
-                    lead_x_change = 10
-                    lead_y_change = 0
-                elif event.key == pygame.K_LEFT:
-                    direction = "left"                    
-                    lead_x_change = -10
-                    lead_y_change = 0
-                elif event.key == pygame.K_UP:
-                    direction = "up"
-                    lead_y_change = -10
-                    lead_x_change = 0
-                elif event.key == pygame.K_DOWN:
-                    direction = "down"
-                    lead_y_change = 10
-                    lead_x_change = 0
-                elif event.key == pygame.K_SPACE:
-                    game_pause()
-
-
-
-        if random_food_x <= lead_x <= random_food_x+20 and random_food_y <= lead_y <= random_food_y+20:
-            random_food_x = round(random.randrange(20,770)/10.0)*10.0
-            random_food_y = round(random.randrange(20,570)/10.0)*10.0
-            snake_len +=1
-
-        if lead_x >= 770 or lead_x <= 20 or lead_y <= 20 or lead_y >= 570:
-            game_over = True
-
-        lead_x += lead_x_change
-        lead_y += lead_y_change
-        game_display.fill(white)
-        game_display.fill(black, rect = [10,10,780,580])
-        game_display.blit(apple_img, [random_food_x,random_food_y,20,20])
-        # game_display.fill(red, rect = [random_food_x,random_food_y,20,20])
-        game_display.fill(white, rect = [lead_x,lead_y,10,10])
-        
-        snake_head = []
-        snake_head.append(lead_x)
-        snake_head.append(lead_y)
-        snake_list.append(snake_head)
-        if len(snake_list) > snake_len:
-            del snake_list[0]
-
-        snake(snake_list,img)
-
-        if len(snake_list) > 2:
-            for tip in snake_list[:-1]:
-                if tip == snake_head:
-                    game_over = True
-
-        score(snake_len - 1)
- 
-        pygame.display.update()
-
-        while game_over:
-            screen_msg("GAME OVER PRESS 'C' FOR CONTINUE AND 'Q' FOR QUIT",red)
-            pygame.display.update()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_c:
-                        run_game()
-                    elif event.key == pygame.K_q:
-                        sys.exit()
-
-        clock.tick(FPS)
-        
+def terminate():
     pygame.quit()
-start()
-# run_game()
+    sys.exit()
+
+
+def checkKeyPress():
+    for event in pygame.event.get():
+        if event.type == KEYUP:
+            if event.key == K_ESCAPE:
+                terminate()
+            else:
+                return True
+
+
+def drawWorm(wormCords):
+    for worm in wormCords:
+        worm_x = worm['x'] * CELLSIZE
+        worm_y = worm['y'] * CELLSIZE
+        pygame.draw.rect(DISPLAYSURF, DARKGREEN, (worm_x, worm_y, CELLSIZE, CELLSIZE))
+        pygame.draw.rect(DISPLAYSURF, GREEN, (worm_x + CELLSIZE//4, worm_y + CELLSIZE//4,math.ceil(CELLSIZE/2),math.ceil(CELLSIZE/2)))
+    pygame.display.update()
+
+
+def showStartScreen():
+    FONT1 = pygame.font.Font("freesansbold.ttf", 100)
+    FONT2 = pygame.font.Font("freesansbold.ttf", 100)
+    angle1 = 15
+    angle2 = 45
+    FONT1SURF = FONT1.render("WORMY!", True, WHITE, DARKGREEN)
+    FONT2SURF = FONT2.render("WORMY!", True, GREEN)
+    while True:
+        if checkKeyPress():
+            pygame.event.get()
+            return
+        
+        DISPLAYSURF.fill(BGCOLOR)
+        ROTATE1SURF = pygame.transform.rotate(FONT1SURF, angle1)
+        ROTATE2SURF = pygame.transform.rotate(FONT2SURF, angle2)
+        FONT1RECT = ROTATE1SURF.get_rect()
+        FONT2RECT = ROTATE2SURF.get_rect()
+        FONT1RECT.center = (WINDOWWIDTH//2, WINDOWHEIGHT//2)
+        FONT2RECT.center = (WINDOWWIDTH//2, WINDOWHEIGHT//2)
+        DISPLAYSURF.blit(ROTATE1SURF, FONT1RECT)
+        DISPLAYSURF.blit(ROTATE2SURF, FONT2RECT)
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+        angle1 += 3
+        angle2 += 7
+
+def drawMsg(msg, fontSize):
+    FONT = pygame.font.Font("freesansbold.ttf", fontSize)
+    FONTSURF = FONT.render(msg, True, WHITE)
+    FONTRECT = FONTSURF.get_rect()
+    # FONTRECT.topleft = (x, y)
+    return FONTSURF, FONTRECT
+
+def gameOverScreen():
+    # scored = SCORE
+    gameOverFont = pygame.font.Font('freesansbold.ttf', 75)
+    gameOverSurf = gameOverFont.render('Game Over', True, WHITE)
+    gameOverRect = gameOverSurf.get_rect()
+    gameOverRect.midtop = (WINDOWWIDTH / 2, 10)
+
+    # scoreSurf, scoreRect = drawMsg("You Scored : " + str(scored))
+    # scoreRect.midtop = (WINDOWWIDTH/2, gameOverRect.height + 25 + 10)
+
+    msg1surf, msg1rect = drawMsg("To Continue.., Press Any Key", 30)
+    msg2surf, msg2rect = drawMsg("To Exit, Press Esc Key", 30)
+    msg1rect.midtop = (WINDOWWIDTH /2, gameOverRect.height + 10 + 25 + 25)
+    msg2rect.midtop = (WINDOWWIDTH /2, gameOverRect.height + msg1rect.height + 10 + 25 + 25 + 25)
+    DISPLAYSURF.blit(gameOverSurf, gameOverRect)
+    # DISPLAYSURF.blit(scoreSurf, scoreRect)
+    DISPLAYSURF.blit(msg1surf, msg1rect)
+    DISPLAYSURF.blit(msg2surf, msg2rect)
+
+    pygame.display.update()
+    pygame.time.wait(500)
+    checkKeyPress() # clear out any key presses in the event queue
+
+    while True:
+        if checkKeyPress():
+            pygame.event.get() # clear event queue
+            return
+
+if __name__ == "__main__":
+    main()
